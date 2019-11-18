5671b8a4bd3d63d549e5d3cc12abf19ee4a925c6
0maru <0maru.dev@gmail.com>
2019-11-19 00:22:16 +0900

    Add WillPopScope

--- lib/src/on_tap_to_unfocus.dart
+++ lib/src/on_tap_to_unfocus.dart
@@ -3,16 +3,32 @@ import 'package:flutter/widgets.dart';
 class OnTapToUnfocus extends StatelessWidget {
   const OnTapToUnfocus({
     Key key,
+    this.androidPrevButton = false,
     @required this.child,
   })  : assert(child != null),
         super(key: key);
+
   final Widget child;
+  final bool androidPrevButton;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       behavior: HitTestBehavior.opaque,
+      child: _buildWillPopScope(context),
+    );
+  }
+
+  Widget _buildWillPopScope(BuildContext context) {
+    if (!androidPrevButton) {
+      return child;
+    }
+    return WillPopScope(
+      onWillPop: () async {
+        FocusScope.of(context).unfocus();
+        return true;
+      },
       child: child,
     );
   }
