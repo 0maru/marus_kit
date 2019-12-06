d56a0e8f4e12944e31eb7a3bf867beb5454f1bfa
0maru <43493921+0maru@users.noreply.github.com>
2019-12-06 10:13:47 +0900

    Update marus_kit.dart
    Fixed

--- lib/marus_kit.dart
+++ lib/marus_kit.dart
@@ -1,7 +1,7 @@
 library marus_kit;
 
 export './src/base_bloc.dart';
-export './src/bloc_provider.dart'
+export './src/bloc_provider.dart';
 export './src/on_tap_to_unfocus.dart';
 export './src/stack_offstage_layer.dart';
 export './src/theme_switcher.dart';
